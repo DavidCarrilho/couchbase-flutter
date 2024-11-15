76026fdde9b0975bd84a04d7f1df5e936a350444
Williambz12 <williamsilva754@gmail.com>
2024-11-15 09:25:23 -0300

    video 1.4

--- lib/app/services/couchbase_service.dart
+++ lib/app/services/couchbase_service.dart
@@ -6,4 +6,16 @@ class CouchbaseService {
   Future<void> init() async {
     database ??= await Database.openAsync('database');
   }
+
+  Future<bool> add({
+    required Map<String, dynamic> data,
+    required String collectionName,
+  }) async {
+    final collection = await database?.createCollection(collectionName);
+    if (collection != null) {
+      final document = MutableDocument(data);
+      return await collection.saveDocument(document);
+    }
+    return false;
+  }
 }
