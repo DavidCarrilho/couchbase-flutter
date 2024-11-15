cd873ed61d0f5f13e3286a94de167303e4246e95
Williambz12 <williamsilva754@gmail.com>
2024-11-15 10:43:23 -0300

    video 5.3

--- lib/app/utils/couchbase_constants.dart
+++ lib/app/utils/couchbase_constants.dart
@@ -1,9 +1,11 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 class CouchbaseContants {
-  static const String userName = 'ChecklistAppUser';
-  static const String password = 'Checklist123.';
+  static String userName = dotenv.get('USER_NAME');
+  static String password = dotenv.get('USER_PASSWORD');
+  static String publicConnectionUrl = dotenv.get('PUBLIC_CONNECTION_URL');
+
   static const String channel = 'checklist_items';
   static const String collection = 'checklist_items';
-  static const String publicConnectionUrl =
-      'wss://a2qoqndw59kr6hsm.apps.cloud.couchbase.com:4984/checklist-app-endpoint';
   static const String scope = 'app_scope';
 }
